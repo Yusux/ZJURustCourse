cf983849e8aa5cfb9020f1944c3e4d63f9e92ad6
YusuxTX <syusux@gmail.com>
2023-09-04 15:53:36 +0800

    [Init] Base for myself

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use regex::Regex;
+use std::env;
+use std::fs;
+use std::path::Path;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 3 {
+        eprintln!("The way to use: {} <target dict> <the regular expression to search>", args[0]);
+        process::exit(1);
+    }
+
+    let pattern = &args[2];
+    let regex = match Regex::new(pattern) {
+        Ok(re) => re,
+        Err(err) => {
+            eprintln!("Invalid regular expression '{}': {}", pattern, err);
+            process::exit(1);
+        }
+    };
+
+    match find(&args[1], &regex) {
+        Ok(matches) => {
+            if matches.is_empty() {
+                println!("No matching file found.");
+            } else {
+                println!("found matching files:");
+                for file in matches {
+                    println!("{}", file);
+                }
+            }
+        }
+        Err(error) => {
+            eprintln!("Error occurred: {}", error);
+            process::exit(1);
+        }
+    }
+}
+
+fn find<P: AsRef<Path>>(root: P, regex: &Regex) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let mut matches = Vec::new();
+    walk_tree(root.as_ref(), regex, &mut matches)?;
+    Ok(matches)
+}
+
+fn walk_tree(
+    dir: &Path,
+    regex: &Regex,
+    matches: &mut Vec<String>
+) -> Result<(), Box<dyn std::error::Error>> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                walk_tree(&path, regex, matches)?;
+            } else if let Some(filename) = path.file_name().and_then(|s| s.to_str()) {
+                if regex.is_match(filename) {
+                    matches.push(path.to_string_lossy().to_string());
+                }
+            }
+        }
+    }
+    Ok(())
+}
